9f34540faff68f8913ed04b543b23b515c634626
MarcosAbue15 <marcosaurelioaquinocardenas@gmail.com>
2026-09-26 12:15:12 -0300

    fix: usa mediaTypes ['images'] (MediaTypeOptions deprecado)

--- mobile/src/app/perfil.tsx
+++ mobile/src/app/perfil.tsx
@@ -73,7 +73,7 @@ export default function PerfilScreen() {
       return;
     }
     const res = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.7,
